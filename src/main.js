831006535687cce0cdb32f4e000a68d2d5e78495
Kai Shoya <s-kai@lumber-mill.co.jp>
2019-05-11 11:55:27 +0900

    firebaseの設定を追加

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+import firebase from 'firebase'
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDY_PiUPLCn4kk7UBmp3vRNUjHBTuGSjW8",
+  authDomain: "chat-app-cdaff.firebaseapp.com",
+  databaseURL: "https://chat-app-cdaff.firebaseio.com",
+  projectId: "chat-app-cdaff",
+  storageBucket: "chat-app-cdaff.appspot.com",
+  messagingSenderId: "162052740584",
+  appId: "1:162052740584:web:f04384381b8f7d22"
+};
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   render: h => h(App),
